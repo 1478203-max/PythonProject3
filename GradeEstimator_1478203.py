1029750d55c2db95dac38ce688e0268c0c1671ec
Rashad Jafarov <rashadjafarov24@hotmail.com>
2026-01-05 01:20:14 -0600

    Auto-update via script

--- GradeEstimator_1478203.py
+++ GradeEstimator_1478203.py
@@ -4,4 +4,11 @@ print ("Hello")
 print ("gotoglu")
 https://github.com/1478203-max/PythonProject3/blob/main/main.py
 
+# Asks the user to input his first name and stores the value under variable called first_name
+first_name = input("Please enter your first name: ")
 
+# Asks the user to input his last name and stores the value under variable called last_name
+last_name = input("Please enter your last name: ")
+
+# Prints the full name by combining both first and last name with a space
+print(first_name + " " + last_name)
