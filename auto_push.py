d0616092226d6d2b4f6d3d3e4fcd712ce646355c
Rashad Jafarov <rashadjafarov24@hotmail.com>
2026-01-06 11:15:30 -0600

    Auto-update via script

--- auto_push.py
+++ auto_push.py
@@ -1,9 +1,8 @@
 x= int(input("what is the value?"))
-while x<5:
+if x<5:
     print ("value is small")
-    break
     elif x>5:
     print (f" value entered {x} is too high")
-else:
+    else:
     print ("value is negative")
 print ("done")
