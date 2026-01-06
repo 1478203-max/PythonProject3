ada78bca818b7e6ff3df4cbee66490681bad3984
Rashad Jafarov <rashadjafarov24@hotmail.com>
2026-01-06 11:10:39 -0600

    Auto-update via script

--- auto_push.py
+++ auto_push.py
@@ -1,7 +1,7 @@
 x= int(input("what is the value?"))
 while x<5:
-    print (value is small)
+    print ("value is small")
     break
 else:
-    print (value is big)
-print (done)
+    print ("value is big")
+print ("done")
