8dee8df528de99cc54474490392bc113ada60fde
Rashad Jafarov <rashadjafarov24@hotmail.com>
2026-01-07 13:29:21 -0600

    Auto-update via script

--- auto_push.py
+++ auto_push.py
@@ -1,7 +1,4 @@
-s = ​"The youngest pope was 11 years old"
-print(s[​0​])       ​# 'T'
-print(s[​1​:​3​])     ​# 'he'
-print(s[​-3​:​-1​])   ​# 'ol'
-print(s[​-3​:])     ​# 'old'
-x = s.split()     ​# creates string array of words
-print(x[​-3​] + ​" "​ + x[​-1​] + ​" "​ + x[​2​] + ​"s"​)  
+s = "The youngest pope was 11 years old"
+
+x = s.split()
+print(x[-3] + " " + x[-1] + " " + x[2] + "s")
