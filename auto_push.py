eff74025dc5cd02f831f9f18295b67086cd8bddc
Rashad Jafarov <rashadjafarov24@hotmail.com>
2026-01-09 12:45:48 -0600

    Auto-update via script

--- auto_push.py
+++ auto_push.py
@@ -14,8 +14,11 @@
 
 # Print the updated list
 #print(numbers)
-fruits = ["apple", "banana", "cherry"]
-for x in fruits:
-  print(x)
-  if x == "banana":
-    break
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([0, 6])
+ypoints = np.array([0, 250])
+
+plt.plot(xpoints, ypoints)
+plt.show()
