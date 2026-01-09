6b0a12b57586288271431f321e2db9bf88f2542a
Rashad Jafarov <rashadjafarov24@hotmail.com>
2026-01-09 12:56:31 -0600

    Auto-update via script

--- auto_push.py
+++ auto_push.py
@@ -22,6 +22,6 @@ ypoints1 = np.array([0, 250])
 xpoints2 = np.array([0, 150])
 ypoints2 = np.array([0, 11])
 
-plt.plot(xpoints1, ypoints1, 'O')
-plt.plot(xpoints2, ypoints2, 'O')
+plt.plot(xpoints1, ypoints1, 'o')
+plt.plot(xpoints2, ypoints2, 'o')
 plt.show()
